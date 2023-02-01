42145a452b30d1518532b93a16879ed2be038034
iqbalaulawi123 <iqbalaulawi123@gmail.com>
2023-02-01 09:50:30 +0700

    development inital

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("TESTING ADD GITHUB REPO TEST");
+        System.out.println("This is a part of development");
     }
 }
